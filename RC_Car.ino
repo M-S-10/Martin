04f95f8068ae57733417e9ac8c7727948699798f
Mehtab <69849348+M-S-10@users.noreply.github.com>
2022-01-18 17:05:42 +0530

    Code written in C/C++
    1. Assigning Motor Driver Pin numbers to interface communication from Arduino
    2. Defining Functions to Control movement towards intended direction
    3. Creating a loop to take continuous input while Bluetooth connection is True
    4. Writing Switch statements within the loop to enable the motors to spin (Via Functions) at command
        A. Controlling spin direction of the motors (PWM)
        B. Controlling speed (H-Bridge)
    
    Voila! There you have it.

---
+++ RC_Car.ino
@@ -0,0 +1,102 @@
+// Arduino RC Car Program //
+
+//L298n Motor Driver pins
+
+#define in1 5 
+#define in2 6
+#define in3 9
+#define in4 10
+
+int command; //To store app command state.
+int Speed = 200; // 0 - 255.
+
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    Stop(); //Initialize with motors stoped.
+    switch (command) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+    
+      case '0':
+        Speed = 100;
+        break;
+      case '1':
+        Speed = 140;
+        break;
+      case '2':
+        Speed = 153;
+        break;
+      case '3':
+        Speed = 165;
+        break;
+      case '4':
+        Speed = 178;
+        break;
+      case '5':
+        Speed = 191;
+        break;
+      case '6':
+        Speed = 204;
+        break;
+      case '7':
+        Speed = 216;
+        break;
+      case '8':
+        Speed = 229;
+        break;
+      case '9':
+        Speed = 242;
+        break;
+      case 'q':
+        Speed = 255;
+        break;
+    }
+  }
+}
+
+void forward() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speed);
+}
+
+void back() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speed);
+}
+
+void left() {
+  analogWrite(in3, Speed);
+  analogWrite(in2, Speed);
+}
+
+void right() {
+  analogWrite(in4, Speed);
+  analogWrite(in1, Speed);
+}
+
+void Stop() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
